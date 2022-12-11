5057e5bac8370be66c384349d2cf1bc6153b7e6b
bambosan <bambosan888@gmail.com>
2022-12-11 17:21:14 +0700

    modified:   index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import qrcode from 'qrcode-terminal';
 import { Boom } from '@hapi/boom';
 import { Configuration, OpenAIApi } from "openai";
 
-const config = new Configuration({ apiKey: process.env });
+const config = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
 const openai = new OpenAIApi(config);
 
 (async function () {
