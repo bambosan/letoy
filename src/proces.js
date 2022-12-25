b1c8446e106c75bed19198e8ca0444cf57679409
bambosan <bambosan888@gmail.com>
2022-12-25 16:38:06 +0700

    modified:   src/proces.js 	modified:   src/sticker.js

--- src/proces.js
+++ src/proces.js
@@ -3,49 +3,81 @@ import { downloadContentFromMessage } from '@adiwajshing/baileys/lib/Utils/messa
 import { dalle, textc } from './openai.js';
 import stickers from './sticker.js';
 import { Buffer } from 'node:buffer';
+import * as fs from 'fs';
 
-const helpMsg = `jawab:
-  letoy jawab pesan..
+const letoyHelp = `letoy <arg>
 
-dalle:
-  letoy dalle pesan..
+list arg:
+• jawab
+• dalle
+• stiker
+`;
 
-stiker:
-  letoy stiker <arg1> <arg2>
-  arg1 (opsional): full, circle, cropped
-  arg2 (opsional): info stiker pack / pesan..
-`
+const stikerHelp = `letoy stiker <arg1> <arg2>
+
+arg1 (opsional):
+• default
+• circle
+• cropped
+
+arg2 (opsional): info stiker / pesan..
+`;
+
 export default async function msgUpsert(socket) {
     socket.ev.on('messages.upsert', async ({ messages }) => {
         if (messages[0].key.remoteJid !== 'status@broadcast') {
-            if (messages[0].message.conversation.match(/^letoy(?= jawab \w)/i)) {
-                textc(socket, messages);
-            }
-
             if(messages[0].message.conversation.match(/^letoy(?= help)/i)){
                 await socket.sendMessage(messages[0].key.remoteJid,
-                    { text: helpMsg },
+                    { text: letoyHelp },
+                    { quoted: messages[0] }
+                );
+            }
+
+            if(messages[0].message.conversation.match(/^letoy(?= stiker help)/i)){
+                await socket.sendMessage(messages[0].key.remoteJid,
+                    { text: stikerHelp },
                     { quoted: messages[0] }
                 );
             }
             
+            if (messages[0].message.conversation.match(/^letoy(?= jawab \w)/i)) {
+                textc(socket, messages);
+            }
+
             if (messages[0].message.conversation.match(/^letoy(?= dalle \w)/i)) {
                 dalle(socket, messages);
             }
             
             if (messages[0].message.imageMessage && messages[0].message.imageMessage.caption.match(/^letoy(?= stiker)/i)) {
                 downloadMediaMessage(messages[0], 'buffer', {}, {}).then(async (buffer) => {
-                    stickers(socket, messages, buffer, true);
+                    stickers(socket, messages, buffer, 'image');
+                });
+            }
+
+            if (messages[0].message.videoMessage && messages[0].message.videoMessage.caption.match(/^letoy(?= stiker)/i)) {
+                downloadMediaMessage(messages[0], 'buffer', {}, {}).then((buffer) => {
+                    stickers(socket, messages, buffer, 'video');
                 });
             }
             
             if (messages[0].message.extendedTextMessage && messages[0].message.extendedTextMessage.text.match(/^letoy(?= stiker)/i)) {
                 if(messages[0].message.extendedTextMessage.contextInfo.quotedMessage.imageMessage){
-                    const imgDat = await downloadContentFromMessage(messages[0].message.extendedTextMessage.contextInfo.quotedMessage.imageMessage, 'image');
+                    const imgStream = await downloadContentFromMessage(messages[0].message.extendedTextMessage.contextInfo.quotedMessage.imageMessage, 'image');
+
+                    const data = [];
+                    imgStream.on('data', (chunk) => data.push(chunk));
+                    imgStream.on('end', () => {
+                        stickers(socket, messages, Buffer.concat(data), 'extimg');
+                    });
+                }
+
+                if(messages[0].message.extendedTextMessage.contextInfo.quotedMessage.videoMessage){
+                    const imgDat = await downloadContentFromMessage(messages[0].message.extendedTextMessage.contextInfo.quotedMessage.videoMessage, 'video');
+
                     const data = [];
                     imgDat.on('data', (chunk) => data.push(chunk));
                     imgDat.on('end', () => {
-                        stickers(socket, messages, Buffer.concat(data), false);
+                        stickers(socket, messages, Buffer.concat(data), 'extvid');
                     });
                 }
             }
--- src/sticker.js
+++ src/sticker.js
@@ -1,21 +1,30 @@
 import { Sticker, StickerTypes } from 'wa-sticker-formatter/dist/index.js';
 
-export default async function stickers(socket, messages, buffers, isImgMsg) {
-    const arg = isImgMsg ? messages[0].message.imageMessage.caption.slice(14).trim() : messages[0].message.extendedTextMessage.text.slice(14).trim();
+export default async function stickers(socket, messages, data, msgtype) {
+    let arg = '';
+    if(msgtype === 'image'){
+        arg = messages[0].message.imageMessage.caption.slice(13).trim();
+    }
+    if(msgtype === 'video'){
+        arg = messages[0].message.videoMessage.caption.slice(13).trim();   
+    }
+    if(msgtype === 'extimg' || msgtype === 'extvid'){
+        arg = messages[0].message.extendedTextMessage.text.slice(13).trim();
+    }
 
     const sticketConfig = {
         pack: arg || 'My Sticker',
         author: messages[0].pushName,
-        type: StickerTypes.DEFAULT,
-        categories: [],
+        type: StickerTypes.FULL,
+        categories: undefined,
         id: String(Math.floor(Math.random() * 1e8)),
-        quality: 50,
+        quality: (msgtype === 'video' || msgtype === 'extvid') ? 1 : 50,
         background: '#000000'
     };
 
-    if (arg.match(/^full/)) {
+    if (arg.match(/^default/)) {
         sticketConfig.pack = arg.slice(5) || 'my sticker';
-        sticketConfig.type = StickerTypes.FULL;
+        sticketConfig.type = StickerTypes.DEFAULT;
     }
 
     if (arg.match(/^circle/)) {
@@ -28,7 +37,7 @@ export default async function stickers(socket, messages, buffers, isImgMsg) {
         sticketConfig.type = StickerTypes.CROPPED;
     }
 
-    const stc = new Sticker(buffers, sticketConfig);
+    const stc = new Sticker(data, sticketConfig);
     await socket.sendMessage(messages[0].key.remoteJid,
         await stc.toMessage(),
         { quoted: messages[0] }
